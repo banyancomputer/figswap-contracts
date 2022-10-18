0dcf09b59b6087403bbaea4cbcea0236f49a620d
Xiangan He <xiangan9@gmail.com>
2022-10-18 15:52:17 -0400

    deploy script debugging > filecoin settings ready

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,6 +30,11 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    wallaby: {
+      url: process.env.WALLABY_URL || "https://wallaby.node.glif.io/rpc/v1",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,6 +12,32 @@ import {
   abi as FACTORY_ABI,
   bytecode as FACTORY_BYTECODE,
 } from "@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json";
+import { NonceManager } from "@ethersproject/experimental";
+
+import RpcEngine from "@glif/filecoin-rpc-client";
+import { newSecp256k1Address } from "@glif/filecoin-address";
+
+require("dotenv").config();
+
+// import { HttpNetworkConfig } from "hardhat/types";
+// import { FeeMarketEIP1559Transaction } from "@ethereumjs/tx";
+
+export const hexlify = (id: string) => {
+  const hexId = Number(id.slice(1)).toString(16);
+  return "0xff" + "0".repeat(38 - hexId.length) + hexId;
+};
+
+export const deriveAddrsFromPk = async (pk: string, apiAddress: string) => {
+  const w = new ethers.Wallet(pk);
+  const pubKey = Uint8Array.from(Buffer.from(w.publicKey.slice(2), "hex"));
+  const secpActor = newSecp256k1Address(pubKey).toString();
+  const filRpc = new RpcEngine({ apiAddress });
+
+  const idActor = await filRpc.request("StateLookupID", secpActor, null);
+  const idActorHex = hexlify(idActor);
+
+  return { secpActor, idActor, idActorHex };
+};
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -22,24 +48,60 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
+  // const [signer] = await ethers.getSigners();
+
+  /*
+  const { secpActor } = await deriveAddrsFromPk(
+    process.env.PRIVATE_KEY!,
+    process.env.WALLABY_URL!
+  );
+  */
+
+  /*
+  const ethRpc = new RpcEngine({
+    apiAddress: network.config.url,
+    namespace: "eth",
+    delimeter: "_",
+  });
+  */
+
+  // const filRpc = new RpcEngine({ apiAddress: network.config.url });
+  const jsonRpcProvider = new ethers.providers.JsonRpcProvider(
+    process.env.WALLABY_URL!
+  );
+  const deployer = new ethers.Wallet(process.env.PRIVATE_KEY!, jsonRpcProvider);
+  const signer = new NonceManager(deployer);
+  const f1addr = await deriveAddrsFromPk(
+    process.env.PRIVATE_KEY!,
+    process.env.WALLABY_URL!
+  );
+
+  console.log(
+    "Your Ethereum(EVM) Address is " +
+      deployer.address +
+      " Your Filecoin Address is " +
+      f1addr
+  );
 
   const Periphery = await ethers.getContractFactory(
     SWAP_ROUTER_ABI,
-    SWAP_ROUTER_BYTECODE
+    SWAP_ROUTER_BYTECODE,
+    signer
   );
 
   const Factory = await ethers.getContractFactory(
     FACTORY_ABI,
-    FACTORY_BYTECODE
+    FACTORY_BYTECODE,
+    signer
   );
 
-  const factory = await Factory.deploy("Hello, Hardhat!");
+  const factory = await Factory.deploy();
   await factory.deployed();
 
-  const periphery = await Periphery.deploy("Hello, Hardhat!");
+  const periphery = await Periphery.deploy();
   await periphery.deployed();
 
-  console.log("Greeter deployed to:", factory.address);
+  console.log("Factory deployed to:", factory.address);
   console.log("Periphery deployed to:", periphery.address);
 }
 
