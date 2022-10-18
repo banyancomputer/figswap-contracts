34bad5fcadc1fd59b7779a988e1630c69c74e38c
Xiangan He <xiangan9@gmail.com>
2022-10-18 14:42:58 -0400

    deploy script ready; dependencies resolving

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,11 +7,11 @@ import { ethers } from "hardhat";
 import {
   abi as SWAP_ROUTER_ABI,
   bytecode as SWAP_ROUTER_BYTECODE,
-} from '@uniswap/v3-periphery/artifacts/contracts/SwapRouter.sol/SwapRouter.json'
+} from "@uniswap/v3-periphery/artifacts/contracts/SwapRouter.sol/SwapRouter.json";
 import {
   abi as FACTORY_ABI,
   bytecode as FACTORY_BYTECODE,
-} from '@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json'
+} from "@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -22,12 +22,25 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  const Periphery = await ethers.getContractFactory(
+    SWAP_ROUTER_ABI,
+    SWAP_ROUTER_BYTECODE
+  );
 
-  console.log("Greeter deployed to:", greeter.address);
+  const Factory = await ethers.getContractFactory(
+    FACTORY_ABI,
+    FACTORY_BYTECODE
+  );
+
+  const factory = await Factory.deploy("Hello, Hardhat!");
+  await factory.deployed();
+
+  const periphery = await Periphery.deploy("Hello, Hardhat!");
+  await periphery.deployed();
+
+  console.log("Greeter deployed to:", factory.address);
+  console.log("Periphery deployed to:", periphery.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
